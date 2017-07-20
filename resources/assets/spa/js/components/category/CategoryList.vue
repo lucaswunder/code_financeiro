27449dc0b0a0ddc7bc67eb1c7f9e3416ed6a356e
Lucas Wundervald <wundervald_2@hotmail.com>
2017-07-20 17:00:33 -0300

    FRONT-END - ADMINISTRAÇÃO DE CATEGORIAS
    listando cat pais e filhas

--- resources/assets/spa/js/components/category/CategoryList.vue
+++ resources/assets/spa/js/components/category/CategoryList.vue
@@ -4,17 +4,35 @@
             <h5>Administração de Categorias</h5>
         </page-title>
         <div class="card-panel z-depth-5">
-
+            <category-tree :categories="categories"></category-tree>
         </div>
     </div>
 </template>
 
 <script>
     import PageTitleComponent from '../PageTitle.vue';
+    import CategoryTreeComponent from '../category/CategoryTree.vue';
+    import {Category} from '../../services/resources';
 
     export default{
         components: {
             'page-title': PageTitleComponent,
+            'category-tree': CategoryTreeComponent,
         },
+        data(){
+            return {
+                categories: []
+            }
+        },
+        created(){
+          this.getCategories();
+        },
+        methods: {
+            getCategories(){
+                Category.query().then(response => {
+                    this.categories = response.data.data;
+                })
+            }
+        }
     }
 </script>
