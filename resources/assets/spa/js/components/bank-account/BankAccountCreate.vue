ac6fe70020622990617bdeb27937ef9b7c2fd569
Lucas Wundervald <wundervald_2@hotmail.com>
2017-07-28 11:42:24 -0300

    VUEX
    Getters

--- resources/assets/spa/js/components/bank-account/BankAccountCreate.vue
+++ resources/assets/spa/js/components/bank-account/BankAccountCreate.vue
@@ -5,14 +5,13 @@
     import {BankAccount} from '../../services/resources';
     import PageTitleComponent from '../PageTitle.vue';
     import 'materialize-autocomplete';
-    import _ from 'lodash';
     import store from '../../store/store';
 
-    export default{
+    export default {
         components: {
             'page-title': PageTitleComponent
         },
-        data(){
+        data() {
             return {
                 title: 'Nova Conta',
                 bankAccount: {
@@ -22,33 +21,33 @@
                     bank_id: '',
                     'default': false,
                 },
-                bank:{
-                  name:''
+                bank: {
+                    name: ''
                 },
                 banks: []
             };
         },
-        computed:{
+        computed: {
             banks() {
                 return store.state.bank.banks;
             }
         },
-        created(){
+        created() {
             this.getBanks();
         },
         methods: {
-            submit(){
+            submit() {
                 BankAccount.save({}, this.bankAccount).then(() => {
                     Materialize.toast('Conta bancária criada com sucesso!', 4000);
                     this.$router.go({name: 'bank-account.list'});
                 })
             },
-            getBanks(){
+            getBanks() {
                 store.dispatch('bank/query').then((response) => {
                     this.initAutoComplete();
                 });
             },
-            initAutoComplete(){
+            initAutoComplete() {
                 let self = this;
                 $(document).ready(() => {
                     $('#bank-id').materialize_autocomplete({
@@ -59,24 +58,17 @@
                         dropdown: {
                             el: '#bank-id-dropdown'
                         },
-                        getData(value, callback){
-                            let banks = self.filterBankByName(value);
-                            banks = banks.map((o) => {
-                                return {id: o.id, text: o.name};
-                            });
+                        getData(value, callback) {
+                            //Pega função declarada no bank  do vuex.
+                            let mapBanks = store.getters['bank/mapBanks'];
+                            let banks = mapBanks(value);
                             callback(value, banks);
                         },
-                        onSelect(item){
+                        onSelect(item) {
                             self.bankAccount.bank_id = item.id;
                         }
                     });
                 });
-            },
-            filterBankByName(name){
-                let banks = _.filter(this.banks, (o) => {
-                    return _.include(o.name.toLowerCase(), name.toLowerCase());
-                });
-                return banks;
             }
         }
     };
--- resources/assets/spa/js/store/bank.js
+++ resources/assets/spa/js/store/bank.js
@@ -1,4 +1,5 @@
 import {Bank} from "../services/resources"
+import _ from 'lodash';
 
 const state = {
     banks: [],
@@ -18,6 +19,25 @@ const actions = {
     }
 };
 
-const module = {namespaced: true, state, mutations, actions};
+const getters = {
+    filterBankByName: (state) => (name) => {
+        let banks = _.filter(state.banks, (o) => {
+            return _.include(o.name.toLowerCase(), name.toLowerCase());
+        });
+        return banks;
+    },
+    mapBanks: (state, getters) => (name) => {
+        let banks = getters.filterBankByName(name);
+        return banks.map((o) => {
+            return {id: o.id, text: o.name}
+        })
+    },
+    banksLength(state) {
+        return state.banks.length
+    }
+};
+
+
+const module = {namespaced: true, state, mutations, actions, getters};
 
 export default module;
